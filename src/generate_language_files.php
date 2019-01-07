a8466ca927ac2e8a32c3e94641ae4a2ae53e819c
vrcs66 <43876629+vrcs66@users.noreply.github.com>
2019-01-07 15:13:39 +0100

    Delete generate_language_files.php

--- src/generate_language_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-chdir(__DIR__);
-
-include('../vendor/autoload.php');
-
-$languageBatchBo = new \Language\LanguageBatchBo();
-$languageBatchBo->generateLanguageFiles();
-$languageBatchBo->generateAppletLanguageXmlFiles();
